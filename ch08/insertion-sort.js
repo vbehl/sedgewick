da46fe204dc93a043abf4d56f266a0fc60091745
Vaishali Behl <vaishalibehl@Vaishalis-MacBook-Pro.local>
2015-12-01 17:37:04 +0530

    implement insertion sort

---
+++ ch08/insertion-sort.js
@@ -0,0 +1,15 @@
+//implement insertion sort
+function sort(array){
+  var length = array.length;
+  for(var i = 1; i < length; i++){
+    var j = i, value = array[j];
+    while(j>0 && value<array[j-1]){
+      array[j] = array[j-1];
+      j--;
+    }
+    array[j] = value;
+  }
+  return array;
+}
+
+console.log(sort([192,2,100,-1]))
